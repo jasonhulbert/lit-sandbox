9fc8e42db55988c6def9acf416dde64a7ca48b1d
Jarrod Zywien <jarrod.zywien@moodys.com>
2020-02-05 17:49:25 -0500

    Initial commit for karma tests

---
+++ src/components/blue-button/blue-button.test.ts
@@ -0,0 +1,11 @@
+// import {BlueButton} from './blue-button';
+
+// import {fixture, elementUpdated } from '@open-wc/testing';
+
+import {expect} from 'chai';
+
+describe('blue-button', () => {
+    it('passes sanity check', () => {
+        expect(true).to.equal(true);
+    });
+});
