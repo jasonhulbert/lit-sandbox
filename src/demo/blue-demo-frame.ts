f5988dda5eab652eaf7108087730c489469ac7e7
jasonhulbert <jason.hulbert@moodys.com>
2020-02-07 13:33:14 -0500

    Adds resizeable container around demo iframe

--- src/demo/blue-demo-frame.ts
+++ src/demo/blue-demo-frame.ts
@@ -5,7 +5,8 @@ import { DemoModule } from './types';
 export class BlueDemoFrame extends LitElement {
     @property({ type: String }) index: string;
 
-    @property({ type: Object }) demoModule: DemoModule;
+    @property({ type: Object })
+    demoModule: DemoModule;
 
     @query('.frame') frame: HTMLIFrameElement;
 
@@ -14,15 +15,43 @@ export class BlueDemoFrame extends LitElement {
     static get styles(): CSSResult {
         return css`
             :host {
-                display: block;
-                width: 100%;
-            }
-
-            .frame {
+                position: absolute;
+                top: 0;
+                left: 0;
                 display: block;
                 border: 0;
                 outline: 0;
                 width: 100%;
+                height: 100%;
+                background: #f1f1f1;
+            }
+
+            .resizer {
+                overflow-x: hidden;
+                overflow-y: scroll;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                display: block;
+                resize: both;
+                width: calc(100% - 4rem);
+                height: calc(100% - 4rem);
+                max-width: calc(100% - 4rem);
+                max-height: calc(100% - 4rem);
+                background: #ffffff;
+                transform: translate(-50%, -50%);
+                box-shadow: 5px 5px 30px -5px rgba(0, 0, 0, 0.2);
+            }
+
+            .frame {
+                position: absolute;
+                top: 0;
+                left: 0;
+                display: block;
+                border: 0;
+                outline: 0;
+                width: 100%;
+                height: 100%;
             }
         `;
     }
@@ -35,7 +64,7 @@ export class BlueDemoFrame extends LitElement {
 
     render(): TemplateResult {
         return html`
-            <div>
+            <div class="resizer">
                 <iframe class="frame" src=${this.index}></iframe>
             </div>
         `;
